06ce37be423567980d8a9af3aff67d7d056fc693
jkonury <jkonury32@gmail.com>
2018-12-13 23:04:49 +0900

    JUnitParams test

--- src/test/java/io/spring/restapi/events/EventTest.java
+++ src/test/java/io/spring/restapi/events/EventTest.java
@@ -2,8 +2,12 @@ package io.spring.restapi.events;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class EventTest {
 
   @Test
@@ -29,14 +33,45 @@ public class EventTest {
   }
 
   @Test
-  public void testFree() {
+  @Parameters({
+    "0, 0, true",
+    "100, 0, false",
+    "0, 100, false",
+  })
+  public void testFree(int basePrice, int maxPrice, boolean isFree) {
+    // Given
     Event event = Event.builder()
-      .basePrice(0)
-      .maxPrice(0)
+      .basePrice(basePrice)
+      .maxPrice(maxPrice)
       .build();
 
+    // When
     event.update();
 
-    assertThat(event.isFree()).isTrue();
+    // Then
+    assertThat(event.isFree()).isEqualTo(isFree);
+  }
+
+  @Test
+  @Parameters
+  public void testOffline(String location, boolean isOffline) {
+    // Given
+    Event event = Event.builder()
+      .location(location)
+      .build();
+
+    // When
+    event.update();
+
+    // Then
+    assertThat(event.isOffline()).isEqualTo(isOffline);
+  }
+
+  private Object[] parametersForTestOffline() {
+    return new Object[] {
+      new Object[] {"강남", true},
+      new Object[] {null, false},
+      new Object[] {"       ", false}
+    };
   }
 }
