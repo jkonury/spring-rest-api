c93bdbfc50ca759e4a0855e76c5febbe81b9ee57
jkonury <jkonury32@gmail.com>
2018-12-13 22:21:08 +0900

    DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES 옵션 enable 입력값이외 값 입력 시 Bad Request(400)

--- src/main/java/io/spring/restapi/Application.java
+++ src/main/java/io/spring/restapi/Application.java
@@ -1,9 +1,12 @@
 package io.spring.restapi;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication
 public class Application {
@@ -15,6 +18,14 @@ public class Application {
   @Bean
   public ModelMapper modelMapper() {
     return new ModelMapper();
+  }
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    return Jackson2ObjectMapperBuilder
+      .json()
+      .featuresToEnable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+      .build();
   }
 }
 
--- src/test/java/io/spring/restapi/events/EventControllerTest.java
+++ src/test/java/io/spring/restapi/events/EventControllerTest.java
@@ -43,6 +43,36 @@ public class EventControllerTest {
   @Test
   public void createEvent() throws Exception {
 
+    EventDto event = EventDto.builder()
+      .name("Spring")
+      .description("REST API Development with Spring")
+      .beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23, 14, 21))
+      .closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 24, 14, 21))
+      .beginEventDateTime(LocalDateTime.of(2018, 11, 25, 14, 21))
+      .endEventDateTime(LocalDateTime.of(2018, 11, 26, 14, 21))
+      .basePrice(100)
+      .maxPrice(200)
+      .limitOfEnrollment(100)
+      .location("Seoul")
+      .build();
+
+    mockMvc.perform(post("/api/events")
+        .contentType(MediaType.APPLICATION_JSON_UTF8)
+        .accept(MediaTypes.HAL_JSON)
+        .content(objectMapper.writeValueAsString(event)))
+      .andDo(print())
+      .andExpect(status().isCreated())
+      .andExpect(jsonPath("id").exists())
+      .andExpect(header().exists(HttpHeaders.LOCATION))
+      .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+      .andExpect(jsonPath("id").value(Matchers.not(10)))
+      .andExpect(jsonPath("free").value(Matchers.not(true)))
+      .andExpect(jsonPath("eventStatus").value(Matchers.is("DRAFT")));
+  }
+
+  @Test
+  public void createEvent_BadRequest() throws Exception {
+
     Event event = Event.builder()
       .id(10L)
       .name("Spring")
@@ -61,17 +91,11 @@ public class EventControllerTest {
       .build();
 
     mockMvc.perform(post("/api/events")
-        .contentType(MediaType.APPLICATION_JSON_UTF8)
-        .accept(MediaTypes.HAL_JSON)
-        .content(objectMapper.writeValueAsString(event)))
+      .contentType(MediaType.APPLICATION_JSON_UTF8)
+      .accept(MediaTypes.HAL_JSON)
+      .content(objectMapper.writeValueAsString(event)))
       .andDo(print())
-      .andExpect(status().isCreated())
-      .andExpect(jsonPath("id").exists())
-      .andExpect(header().exists(HttpHeaders.LOCATION))
-      .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-      .andExpect(jsonPath("id").value(Matchers.not(10)))
-      .andExpect(jsonPath("free").value(Matchers.not(true)))
-      .andExpect(jsonPath("eventStatus").value(Matchers.is("DRAFT")))
-    ;
+      .andExpect(status().isBadRequest());
   }
+
 }
