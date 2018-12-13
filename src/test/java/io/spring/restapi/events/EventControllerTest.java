883ffefdfe80193c5991a753187f2c84cb2c6410
jkonury <jkonury32@gmail.com>
2018-12-13 21:16:23 +0900

    EventControllerTest add

---
+++ src/test/java/io/spring/restapi/events/EventControllerTest.java
@@ -0,0 +1,62 @@
+package io.spring.restapi.events;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDateTime;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class EventControllerTest {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Autowired
+  ObjectMapper objectMapper;
+
+  @Test
+  public void createNullEvent() throws Exception {
+    mockMvc.perform(post("/api/events")
+      .contentType(MediaType.APPLICATION_JSON_UTF8)
+      .accept(MediaTypes.HAL_JSON))
+      .andDo(print())
+      .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  public void createEvent() throws Exception {
+
+    Event event = Event.builder()
+      .name("Spring")
+      .description("REST API Development with Spring")
+      .beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23, 14, 21))
+      .closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 24, 14, 21))
+      .beginEventDateTime(LocalDateTime.of(2018, 11, 25, 14, 21))
+      .endEventDateTime(LocalDateTime.of(2018, 11, 26, 14, 21))
+      .basePrice(100)
+      .maxPrice(200)
+      .limitOfEnrollment(100)
+      .location("Seoul")
+      .build();
+
+    mockMvc.perform(post("/api/events")
+        .contentType(MediaType.APPLICATION_JSON_UTF8)
+        .accept(MediaTypes.HAL_JSON)
+        .content(objectMapper.writeValueAsString(event)))
+      .andDo(print())
+      .andExpect(status().isCreated())
+      .andExpect(jsonPath("id").exists());
+  }
+}
