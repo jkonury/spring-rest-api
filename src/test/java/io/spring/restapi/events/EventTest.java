125b6945accbb43ca35eb33cafed0a4453f69b3a
jkonury <jkonury32@gmail.com>
2018-12-13 20:54:01 +0900

    Event domain add

---
+++ src/test/java/io/spring/restapi/events/EventTest.java
@@ -0,0 +1,30 @@
+package io.spring.restapi.events;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class EventTest {
+
+  @Test
+  public void builder() {
+    final Event event = Event.builder()
+      .name("Spring REST API")
+      .description("REST API development with Spring")
+      .build();
+    assertThat(event).isNotNull();
+  }
+
+  @Test
+  public void javaBean() {
+    Event event = new Event();
+    String name = "event";
+    String description = "Spring";
+
+    event.setName(name);
+    event.setDescription(description);
+
+    assertThat(event.getName()).isEqualTo(name);
+    assertThat(event.getDescription()).isEqualTo(description);
+  }
+}
