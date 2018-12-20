760e1df0da52674e986934a7c0f311b142f7c053
jkonury <jkonury32@gmail.com>
2018-12-20 14:36:08 +0900

    test code refactoring

--- src/test/java/io/spring/restapi/events/EventControllerTest.java
+++ src/test/java/io/spring/restapi/events/EventControllerTest.java
@@ -17,46 +17,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.spring.restapi.common.RestDocsConfiguration;
+import io.spring.restapi.common.BaseControllerTest;
 import io.spring.restapi.common.TestDescription;
 import java.time.LocalDateTime;
 import java.util.stream.IntStream;
 import org.hamcrest.Matchers;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
-@ActiveProfiles("test")
-public class EventControllerTest {
-
-  @Autowired
-  MockMvc mockMvc;
-
-  @Autowired
-  ObjectMapper objectMapper;
+public class EventControllerTest extends BaseControllerTest {
 
   @Autowired
   EventRepository eventRepository;
-
-  @Autowired
-  ModelMapper modelMapper;
 
   @Test
   public void createNullEvent() throws Exception {
