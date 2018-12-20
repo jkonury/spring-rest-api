e62aed629368b3015117fbf881dfede5e45a91a4
jkonury <jkonury32@gmail.com>
2018-12-21 08:47:42 +0900

    spring security - basic config

--- src/main/java/io/spring/restapi/Application.java
+++ src/main/java/io/spring/restapi/Application.java
@@ -1,20 +1,13 @@
 package io.spring.restapi;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
-  }
-
-  @Bean
-  public ModelMapper modelMapper() {
-    return new ModelMapper();
   }
 
 }
