7fd1e011763cfc0082e9d7b53c2ab2dcf29d54b8
jkonury <jkonury32@gmail.com>
2022-11-29 23:07:51 +0900

    fix: remove warning

--- src/main/java/io/spring/restapi/index/IndexController.java
+++ src/main/java/io/spring/restapi/index/IndexController.java
@@ -3,6 +3,7 @@ package io.spring.restapi.index;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 import io.spring.restapi.events.EventController;
+import java.util.List;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-  @GetMapping("/api")
-  public RepresentationModel index() {
-    var index = new RepresentationModel();
-    index.add(linkTo(EventController.class).withRel("events"));
-    return index;
+  @GetMapping({"/api", "/api/"})
+  public RepresentationModel<?> index() {
+   return new RepresentationModel<>(List.of(linkTo(EventController.class).withRel("events")));
   }
 }
